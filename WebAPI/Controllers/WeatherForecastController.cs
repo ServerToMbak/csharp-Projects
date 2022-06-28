5fe42172f10127689fa6f4fd98cd98eacec990da
ServerToMbak <server78135@gmail.com>
2022-06-28 15:23:27 +0300

    update

--- WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebAPI.Controllers
 { 
     [ApiController]
-    [Route("[controller]")]asdsa
+    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
