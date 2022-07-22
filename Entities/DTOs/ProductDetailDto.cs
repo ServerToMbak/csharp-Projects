63a0473a3cc8ed4ebad92d02dc566e19e0b7a1bb
ServerToMbak <server78135@gmail.com>
2022-07-22 11:24:52 +0300

    Updates

--- Entities/DTOs/ProductDetailDto.cs
+++ Entities/DTOs/ProductDetailDto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.DTOs
 {
-    public class ProductDetailDto:IDto
+    public class ProductDetailDto:IDto 
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
